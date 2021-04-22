16ed64cd996769254e6bb3e17c1be5e2e3b4ff27
zhaoxia413 <zhaoxia413@163.com>
2021-04-23 06:10:48 +0800

    correlate the BMI and irAEs analysis

--- R_function/surv_plot.R
+++ R_function/surv_plot.R
@@ -5,15 +5,16 @@ surv_plot<-function(fit,data,colors,title){
   library(grid)
   library(ggplotify)
   p<-ggsurvplot(fit, data=data,xlab = "Time(months)", linetype = "strata",
-                legend.title = "",censor.size=0.5, size = 0.5,
+                legend.title = "",censor.size=0.3, size = 0.3,
                 risk.table = F,
+                tables.theme = theme_few(base_size = 6),
                 #legend = c(0.84, 0.8),
-                pval = TRUE,pval.size = 4, 
+                pval = TRUE,pval.size = 2, 
                 pval.coord=c(0.8,0.2),pval.method=F,
                 pval.method.coord=c(0.05,0.3), 
                 ggtheme = theme_minimal() + 
                   theme(line = element_line(size = 0.1),
-                        text  = element_text(size = 8),
+                        text  = element_text(size = 6),
                         legend.direction = "vertical"),
                 palette = colors,
                 main=title,
